8849ed8dc55ed55acb1e91fde075149b030367ce
Mohamed Meabed <mo.meabed@gmail.com>
2017-12-28 00:01:13 +0400

    Fix tests, callbacks, binary

--- engines/Mongodb.js
+++ engines/Mongodb.js
@@ -2,8 +2,8 @@
 // Copyright (c) 2015 Joseph Huckaby
 // Released under the MIT License
 
-// Requires the 'mongodb' module from npm
-// npm install mongodb
+// Requires the 'monk' module from npm
+// npm install monk
 
 var Class = require("pixl-class");
 var Component = require("pixl-server/component");
