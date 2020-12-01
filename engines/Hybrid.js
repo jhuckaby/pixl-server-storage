da189a4968a438c8bb5ab946fdb7d41c54f52e11
Joseph Huckaby <jhuckaby@gmail.com>
2020-12-01 09:39:04 -0800

    Added new Hybrid engine.

---
+++ engines/Hybrid.js
@@ -0,0 +1,108 @@
+// Hybrid Storage Plugin
+// Copyright (c) 2015 - 2020 Joseph Huckaby
+// Released under the MIT License
+
+var Class = require("pixl-class");
+var Component = require("pixl-server/component");
+var Tools = require("pixl-tools");
+var async = require('async');
+
+module.exports = Class.create({
+	
+	__name: 'Hybrid',
+	__parent: Component,
+	
+	defaultConfig: {
+		binaryEngine: "",
+		docEngine: ""
+	},
+	
+	startup: function(callback) {
+		// setup multiple sub-engines
+		this.logDebug(2, "Setting up hybrid engine", this.config.get() );
+		
+		this.binaryEngine = new require( './' + this.config.get('binaryEngine') + '.js' )();
+		this.binaryEngine.storage = this.storage;
+		this.binaryEngine.init( this.server, this.storage.config.getSub( this.config.get('binaryEngine') ) );
+		
+		this.docEngine = new require( './' + this.config.get('docEngine') + '.js' )();
+		this.docEngine.storage = this.storage;
+		this.docEngine.init( this.server, this.storage.config.getSub( this.config.get('docEngine') ) );
+		
+		async.series([
+			this.binaryEngine.startup.bind(this.binaryEngine),
+			this.docEngine.startup.bind(this.docEngine)
+		], callback );
+	},
+	
+	put: function(key, value, callback) {
+		// store key+value in hybrid system
+		if (this.storage.isBinaryKey(key)) {
+			this.binaryEngine.put( key, value, callback );
+		}
+		else {
+			this.docEngine.put( key, value, callback );
+		}
+	},
+	
+	putStream: function(key, inp, callback) {
+		// store key+value in hybrid system using read stream
+		// streams are binary only!
+		this.binaryEngine.putStream( key, inp, callback );
+	},
+	
+	head: function(key, callback) {
+		// head hybrid value given key
+		if (this.storage.isBinaryKey(key)) {
+			this.binaryEngine.head( key, callback );
+		}
+		else {
+			this.docEngine.head( key, callback );
+		}
+	},
+	
+	get: function(key, callback) {
+		// fetch hybrid value given key
+		if (this.storage.isBinaryKey(key)) {
+			this.binaryEngine.get( key, callback );
+		}
+		else {
+			this.docEngine.get( key, callback );
+		}
+	},
+	
+	getStream: function(key, callback) {
+		// get readable stream to record value given key
+		// streams are binary only!
+		this.binaryEngine.getStream( key, callback );
+	},
+	
+	delete: function(key, callback) {
+		// delete hybrid key given key
+		if (this.storage.isBinaryKey(key)) {
+			this.binaryEngine.delete( key, callback );
+		}
+		else {
+			this.docEngine.delete( key, callback );
+		}
+	},
+	
+	runMaintenance: function(callback) {
+		// run daily maintenance
+		async.series([
+			this.binaryEngine.runMaintenance.bind(this.binaryEngine),
+			this.docEngine.runMaintenance.bind(this.docEngine)
+		], callback );
+	},
+	
+	shutdown: function(callback) {
+		// shutdown storage
+		this.logDebug(2, "Shutting down hybrid system");
+		
+		async.series([
+			this.binaryEngine.shutdown.bind(this.binaryEngine),
+			this.docEngine.shutdown.bind(this.docEngine)
+		], callback );
+	}
+	
+});
